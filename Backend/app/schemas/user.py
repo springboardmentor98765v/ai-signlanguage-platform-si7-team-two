0de642b706b29e7972f81a9a6762746bd741fed4
pooja-ch-132 <poojithachougani@gmail.com>
2026-07-10 14:14:52 +0530

    Day 3: Added authentication structure and utilities

---
+++ Backend/app/schemas/user.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+
+
+class UserRegister(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+    role_id: UUID
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: UUID
+    full_name: str
+    email: EmailStr
+    role_id: UUID
+
+    class Config:
+        from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
